08f77863dc5415828c8c1a468b562d52ef80493e
AlexanderSV91 <alexander.shalamov@ukr.net>
2024-04-26 19:00:08 +0200

    created config-service

---
+++ config-service/src/test/java/com/github/alexandersv91/printempafolia/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alexandersv91.printempafolia.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
